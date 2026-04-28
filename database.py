22707ae71b4ee0df802fbd982abefc0e2981f54d
Fernando Azevedo Jr <fernandojr@azevedoefilhos.com.br>
2026-04-28 19:25:46 -0300

    Fix regex _cols: remover parenteses extras que criavam grupos extras

--- database.py
+++ database.py
@@ -98,7 +98,7 @@ if _USE_SUPABASE:
         sql = sql.replace("IFNULL(", "COALESCE(")
         # Cast colunas TEXT de data para DATE nas comparacoes
         # Usa lookbehind para capturar apenas quando precedida de ponto (alias.coluna)
-        _cols = r"(data_pedido|data_contato|data_pesquisa|data_followup|data_entrega|data_visita|data_pagamento|data_inicio|data_fim|data_registro|data_vigencia|data_upload)"
+        _cols = r"data_pedido|data_contato|data_pesquisa|data_followup|data_entrega|data_visita|data_pagamento|data_inicio|data_fim|data_registro|data_vigencia|data_upload"
         sql = re.sub(rf"\.({_cols})\s*(>=|<=|=|>|<)", r".\1::DATE \2", sql)
         sql = re.sub(rf"\.({_cols})\s+BETWEEN", r".\1::DATE BETWEEN", sql)
         result = []; i = 0; sql_up = sql.upper()
