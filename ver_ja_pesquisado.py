d34c76aac6511fd9f7b3713852dc284e0a72d108
Fernando Azevedo Jr <fernandojr@azevedoefilhos.com.br>
2026-05-03 15:02:05 -0300

    Limpeza: remover scripts temporarios

--- ver_ja_pesquisado.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import pathlib
-src = pathlib.Path("pesquisa.py").read_text(encoding="utf-8")
-lines = src.splitlines()
-for i, l in enumerate(lines, 1):
-    if '_confirmar_key' in l or '_ja_existe' in l:
-        print(i, lines[i-1][:85].encode('ascii','replace').decode())
