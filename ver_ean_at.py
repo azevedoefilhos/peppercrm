a24a902a9ee10385babda36a585e8dc76b134bf4
Fernando Azevedo Jr <fernandojr@azevedoefilhos.com.br>
2026-05-04 17:46:44 -0300

    Limpeza: remover scripts temporarios

--- ver_ean_at.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import pathlib
-src = pathlib.Path("concorrentes.py").read_text(encoding="utf-8")
-lines = src.splitlines()
-for i in range(620, 665):
-    print(i+1, lines[i][:90].encode('ascii','replace').decode())
