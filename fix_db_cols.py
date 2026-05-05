bee1260d0ac17297fe935e123f906dcd6a942658
Fernando Azevedo Jr <fernandojr@azevedoefilhos.com.br>
2026-05-05 15:50:09 -0300

    Limpeza: remover scripts temporarios

--- fix_db_cols.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import pathlib, re
-
-src = pathlib.Path("database.py").read_text(encoding="utf-8")
-
-# Ver contexto do regex com _cols
-idx = src.find('vigencia|data_upload')
-print(src[idx-300:idx+300])
