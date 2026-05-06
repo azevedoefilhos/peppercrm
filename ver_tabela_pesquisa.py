16686db0dd9be8e6d7eb7e822a8a3adef848a00d
Fernando Azevedo Jr <fernandojr@azevedoefilhos.com.br>
2026-05-06 16:26:11 -0300

    Limpeza: remover scripts temporarios

--- ver_tabela_pesquisa.py
+++
@@ -1,7 +0,0 @@
-from database import query
-cols = query("""SELECT column_name, data_type 
-    FROM information_schema.columns 
-    WHERE table_name='pesquisa_preco_item' 
-    ORDER BY ordinal_position""")
-for c in cols:
-    print(c[0], c[1])
