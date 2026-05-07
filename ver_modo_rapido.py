1d9a23f081695327625703e97ee4ffa0e65c3cf6
Fernando Azevedo Jr <fernandojr@azevedoefilhos.com.br>
2026-05-07 12:49:08 -0300

    Limpeza: remover scripts temporarios

--- ver_modo_rapido.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import pathlib
-src = pathlib.Path("pesquisa.py").read_text(encoding="utf-8")
-lines = src.splitlines()
-
-# Ver a parte onde os botoes de produto sao renderizados
-for i in range(3760, 3850):
-    print(i+1, lines[i][:90].encode('ascii','replace').decode())
